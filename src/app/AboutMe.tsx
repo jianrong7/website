3ba533594697c6ea2957f86460611b795f845326
jianrong7_ <jianrong@99.co>
2023-08-06 18:13:48 +0800

    add some margin

--- src/app/AboutMe.tsx
+++ src/app/AboutMe.tsx
@@ -3,11 +3,11 @@ export default function AboutMe() {
     <>
       <h2 className="mt-6 text-xl font-semibold">About me</h2>
       <ul>
-        <li>
+        <li className="mb-2">
           I am currently a Computer Science sophomore from National University
           of Singapore (NUS).
         </li>
-        <li>
+        <li className="mb-2">
           I am a tech lead and software engineer at{" "}
           <a
             href="https://dsc.comp.nus.edu.sg/"
@@ -19,11 +19,11 @@ export default function AboutMe() {
           . I have built software for Diabetes Singapore and am currently
           building a solution for Caregivers Alliance.
         </li>
-        <li>
+        <li className="mb-2">
           I am a full-stack software engineer, with professional experience
           mostly in web development.
         </li>
-        <li>
+        <li className="mb-2">
           I have strong domain knowledge in the Southeast Asian real estate
           market and sustainable finance. I am also adept at creating SEO
           friendly sites.
--- src/app/TechnicalStuff.tsx
+++ src/app/TechnicalStuff.tsx
@@ -9,18 +9,18 @@ export default function TechnicalStuff() {
           meta-frameworks built around it. I prefer to use Next.js and
           TypeScript.
         </li>
-        <li>
+        <li className="mb-2">
           On the backend, I build projects with a variety of languages and
           frameworks, ranging from Node.js, to Python, to Golang. I would say
           Python&apos;s FastAPI has provided me with the best developer
           experience so far.
         </li>
-        <li>
+        <li className="mb-2">
           I am also deeply interested in software architecture and making sure
           they are scalable. I prefer to find the right tool for the job,
           instead of trying to force-fit microservices into every project I do.
         </li>
-        <li>
+        <li className="mb-2">
           I have also recently tried out prompt engineering, a passion fueled by
           the arrival of ChatGPT, the release of Meta&apos;s LLM, Llama2, and
           the rising popularity of LangChain.
